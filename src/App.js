5bcdaa6e5994b917ab5fd3c3cf1f05253f1425a9
Rodrigo Carnevale <53884691+rcarnevale@users.noreply.github.com>
2019-10-31 18:43:29 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
 import React from 'react';
-import './App.css';
+import './css/pure-min.css';
+import './css/side-menu.css';
+import {Link} from 'react-router-dom';
 
-import Tabela from './componentes/Tabela';
 
-function App() {
-  const autores =[
-    {
-      nome: 'Paulo',
-      livro: 'React',
-      preco: '1000'
-    },
-    {
-      nome: 'Daniel',
-      livro: 'Java',
-      preco: '99'
-    },
-    {
-      nome: 'Marcos',
-      livro: 'Design',
-      preco: '150'
-    },
-    {
-      nome: 'Bruno',
-      livro: 'DevOps',
-      preco: '100'
-    }
-  ]
+class App extends React.Component{
 
-  return (
-    <div className="App">
-      <Tabela autores= {autores}/>
+  render(){
+      return(
+    <div id="layout">
+    
+    <a href="#menu" id="menuLink" className="menu-link">
+        
+        <span></span>
+    </a>
+
+    <div id="menu">
+        <div className="pure-menu">
+            <a className="pure-menu-heading" href="#">Livraria</a>
+
+            <ul className="pure-menu-list">
+                <li className="pure-menu-item"><Link to="/" className="pure-menu-link">Home</Link></li>
+                <li className="pure-menu-item"><Link to="/autor" className="pure-menu-link">Autor</Link></li>
+                <li className="pure-menu-item"><Link to="/livro" className="pure-menu-link">Livros</Link></li>
+            </ul>
+        </div>
     </div>
+
+    <div id="main">
+           {this.props.children}>
+    </div>         
+</div>
   );
+}
 }
 
 export default App;
