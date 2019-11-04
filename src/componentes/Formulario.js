5d36710327fa5604dd4a313f4ad9180613690aec
Rodrigo Carnevale <53884691+rcarnevale@users.noreply.github.com>
2019-11-04 00:09:41 -0300

    Add files via upload
    Projeto estilizado com materializeCss

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -32,39 +32,40 @@ class Formulario extends React.Component{
 
         return(
             <form>
-
-            <label htmlFor="nome">Nome</label>
-            <input
-                id="nome"
-                type="text"
-                name="nome"
-                value={nome}
-                onChange={this.inputFormulario}
-                />
-
-            <label htmlFor="livro">Livro</label>
-            <input
-                id="livro"
-                type="text"
-                name="livro"
-                value={livro}
-                onChange={this.inputFormulario}
-                />
-
-
-            <label htmlFor="preco">Preço</label>
-            <input
-                id="preco"
-                type="text"
-                name="preco"
-                value={preco}
-                onChange={this.inputFormulario}
-                />
-
-
-            <button type="button" onClick={this.submitForm}>Salvar
-            </button>
-        </form>
+                <div className="row">
+                    <div className="input-field col s4">
+                        <label htmlFor="nome">Nome</label>
+                        <input
+                            id="nome"
+                            type="text"
+                            name="nome"
+                            value={nome}
+                            onChange={this.inputFormulario}
+                        />
+                    </div>
+                    <div className="input-field col s4">
+                        <label htmlFor="livro">Livro</label>
+                        <input
+                            id="livro"
+                            type="text"
+                            name="livro"
+                            value={livro}
+                            onChange={this.inputFormulario}
+                        />
+                    </div>
+                    <div className="input-field col s4">
+                        <label htmlFor="preco">Preço</label>
+                        <input
+                            id="preco"
+                            type="text"
+                            name="preco"
+                            value={preco}
+                            onChange={this.inputFormulario}
+                        />
+                    </div>
+                </div>
+                <button className="waves-effect waves-light btn indigo lighten-3" type="button" onClick={this.submitForm}>Salvar</button>
+            </form>
         )
     }
 }
--- src/componentes/Tabela.js
+++ src/componentes/Tabela.js
@@ -3,30 +3,32 @@ import React from 'react';
 const TableHead = () => {
     return(
         <thead>
-          <tr>
-            <th>Autores</th>
-            <th>Livros</th>
-            <th>Preços</th>
-            <th>Remover</th>
-          </tr>
+            <tr>
+                <th>Autores</th>
+                <th>Livros</th>
+                <th>Preços</th>
+                <th>Remover</th>
+            </tr>
         </thead>
     )
 }
 
 const TableBody = (props) => {
     const linhas = props.autores.map( (linha,index) => {
-        return <tr key={index}>
-            <td>{linha.nome}</td>
-            <td>{linha.livro}</td>
-            <td>{linha.preco}</td>
-            <td><button onClick={() => {props.removeAutor(index)}}>Remover</button></td>
-        </tr>
+        return(
+            <tr> 
+                <td>{linha.nome}</td>
+                <td>{linha.livro}</td>
+                <td>{linha.preco}</td>
+                <td><button className="waves-effect waves-light btn indigo lighten-3" onClick={() => {props.removeAutor(index)}}>Remover</button></td>
+            </tr>
+        )
     })
 
     return(
-        <tb>
+        <tbody>
             {linhas}
-        </tb>
+        </tbody>
     )
 }
 
@@ -34,7 +36,7 @@ class Tabela extends React.Component{
     render(){
         const {autores, removeAutor} = this.props;
         return (
-            <table>
+            <table className="centered highlight">
                 <TableHead />
                 <TableBody autores = {autores} removeAutor = {removeAutor}/>
             </table>
