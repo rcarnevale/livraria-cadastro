4e0053e4415c675eba086f0c9c85dbdb8d73c282
Rodrigo Carnevale <53884691+rcarnevale@users.noreply.github.com>
2019-11-10 21:03:20 -0300

    Add files via upload

---
+++ build/precache-manifest.fcfc5797f358e20276353b613d6d7f35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ff2b55d949231678f0875ec88036c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cef76e24e5f7e63a1173",
+    "url": "/static/css/main.da254ed1.chunk.css"
+  },
+  {
+    "revision": "a399d214fba94ca5c151",
+    "url": "/static/js/2.c3689c0e.chunk.js"
+  },
+  {
+    "revision": "cef76e24e5f7e63a1173",
+    "url": "/static/js/main.17c8cefb.chunk.js"
+  },
+  {
+    "revision": "8c7f4adc1c57d51bd20b",
+    "url": "/static/js/runtime-main.42a1bd2e.js"
+  }
+]);
